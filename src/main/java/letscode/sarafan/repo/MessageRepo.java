64b47a7dd4883aacda3e7de73cafd772eeb608d3
fz.bochkarev <H_y678ik>
2020-05-12 15:41:36 +0200

    Подключаем базу данных, настраиваем Jackson. Spring Boot REST

---
+++ src/main/java/letscode/sarafan/repo/MessageRepo.java
@@ -0,0 +1,9 @@
+package letscode.sarafan.repo;
+
+import letscode.sarafan.domain.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface MessageRepo extends JpaRepository<Message, Long> {
+
+}
