8a6d4a8e65015e22c17e9c54c088ca4001b5f0dd
fz.bochkarev <H_y678ik>
2020-05-14 08:03:59 +0200

    Настраиваем Spring Security и oAuth2 в Vue.js. Spring Boot REST

--- src/main/java/letscode/sarafan/config/WebSecurityConfig.java
+++ src/main/java/letscode/sarafan/config/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 import java.time.LocalDateTime;
 
@@ -19,9 +20,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .antMatcher("/**")
                 .authorizeRequests()
-                .mvcMatchers("/").permitAll()
+                .antMatchers("/", "/login**", "/js/**", "/error**").permitAll()
                 .anyRequest().authenticated()
+                .and()
+                .logout().logoutSuccessUrl("/").permitAll()
                 .and()
                 .csrf().disable();
     }
