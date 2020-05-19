b17230f9184d1706517cdaa838ce76553b37d26b
fz.bochkarev <H_y678ik>
2020-05-19 09:02:10 +0200

    Стилизуем приложение с Vuetify (Material design). Spring Boot REST

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,16 +1,19 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
+import Vuetify from 'vuetify'
 import App from 'pages/App.vue'
-import { connect } from "./util/ws";
+import { connect } from './util/ws'
+import 'vuetify/dist/vuetify.min.css'
 
 if (frontendData.profile) {
-    connect();
+    connect()
 }
 
+Vue.use(Vuetify)
 Vue.use(VueResource)
 
 new Vue({
     el: '#app',
+    vuetify: new Vuetify(),
     render: a => a(App)
 })
-
